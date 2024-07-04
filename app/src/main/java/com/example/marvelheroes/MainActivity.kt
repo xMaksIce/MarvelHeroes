7892a9932a5a9f66b45614f820fe6b5dba91ccbf
Maksim <maksiceofficial@gmail.com>
2024-07-04 23:29:37 +0600

    feat: add main activity

--- app/src/main/java/com/example/marvelheroes/MainActivity.kt
+++ app/src/main/java/com/example/marvelheroes/MainActivity.kt
@@ -4,44 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.marvelheroes.ui.theme.MarvelHeroesTheme
+import com.example.marvelheroes.ui.screens.Home
+import com.example.marvelheroes.ui.screens.GreenGoblin
+import com.example.marvelheroes.ui.screens.Kraven
+import com.example.marvelheroes.ui.screens.Electro
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             MarvelHeroesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("Home") { Home(navController)}
+                    composable("Green Goblin") { GreenGoblin(navController) }
+                    composable("Kraven") { Kraven(navController) }
+                    composable("Electro") { Electro(navController) }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MarvelHeroesTheme {
-        Greeting("Android")
     }
 }
